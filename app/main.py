19adb5e9fd51aba94502b0bc1b0086c563ff7475
Yash <yash.fullstackdev@gmail.com>
2025-06-11 16:28:39 +0530

    fix: port fix

--- app/main.py
+++ app/main.py
@@ -107,5 +107,7 @@ if __name__ == "__main__":
     import uvicorn
     import os
 
-    port = int(os.environ.get("PORT", 8000))  # default to 8000 locally
+    # port = int(os.environ.get("PORT", 8000))  # default to 8000 locally
+    port = int(os.getenv("PORT", 3000))
+
     uvicorn.run(app, host="0.0.0.0", port=port)
